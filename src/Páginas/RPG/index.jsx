dc936088839f55119d902d3322b106b609ee471c
Sidnei Apontes <95299443+aponntes@users.noreply.github.com>
2022-09-20 22:58:27 -0300

    19/09

--- src/Páginas/RPG/index.jsx
+++ src/Páginas/RPG/index.jsx
@@ -1,47 +1,10 @@
 import React from 'react';
-import './rpg.css'
 import { Link } from 'react-router-dom';
 
-export default function RPG(){
+export default function Rpg(){
     return(
-        <div className='container'>
-
-    <div className='mainbox'>
-        <div className='navbar'>
-            <button className='navmoba'>
-                <Link to="/" className='Link'>
-                    Home
-                </Link>
-            </button>
-            <button className='navmoba'>
-                <Link to="/rpg" className='Link'>
-                    Início
-                </Link>
-            </button>
-
-            <button className='navmoba'>
-                <Link to="/mine" className='Link'>
-                    MINECRAFT
-                </Link>
-            </button>
-            
-            <button className='navmoba'>
-                <Link to="/gta" className='Link'>
-                    GTA RP
-                </Link>
-            </button>
-
-            <button className='navmoba'>
-                <Link to="/roblox" className='Link'>
-                    ROBLOX
-                </Link>
-            </button>
-
-            
+        <div>
+            <p>Página de rpg</p>
         </div>
-        <p> só pra confirmar que chegou em Início do Roblox</p>
-    </div>
-
-</div>
     )
 }
--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -9,10 +9,6 @@ import Fps from './Páginas/FPS';
 import LOL from './Páginas/MOBA/Páginas/LOL/LOL';
 import MobileLegends from './Páginas/MOBA/Páginas/MobileLegends/MobileLegends';
 import PokemonUnite from './Páginas/MOBA/Páginas/PokemonUnite/PokemonUnite';
-import Minecraft from './Páginas/RPG/Páginas/Minecraft/Minecraft';
-import Gta from './Páginas/RPG/Páginas/Gta/Gta';
-import Roblox from './Páginas/RPG/Páginas/Roblox/Roblox';
-
 
 export default function Rotas() {
   return (
@@ -27,10 +23,6 @@ export default function Rotas() {
         <Route path="/lol" element={<LOL />} />
         <Route path="/mobilelegends" element={<MobileLegends />} />
         <Route path="/pokemonunite" element={<PokemonUnite />} />
-        <Route path="/mine" element={<Minecraft />} />
-        <Route path='/gta' element={<Gta />} />
-        <Route path='/roblox' element={<Roblox />} />
-
       </Routes>
     </BrowserRouter>
   )
