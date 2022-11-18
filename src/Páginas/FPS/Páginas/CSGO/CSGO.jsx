aea472fbea77a3253b3ffc8f4a32b0564f6e7023
BelchiorLikesWhales <106777905+BelchiorLikesWhales@users.noreply.github.com>
2022-11-18 14:28:45 -0400

    Add files via upload

---
+++ src/Páginas/FPS/Páginas/CSGO/CSGO.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import './CSGO.css';
+import { Link } from 'react-router-dom';
+
+
+
+export default function CSGO(){
+    return(
+    <div className='container'>
+
+        <div className='mainbox'>
+            <div className='navbar'>
+            <Link to="/" className='Link'>
+                <button className='navmoba'>
+                        Home
+                </button>
+            </Link>
+
+            <Link to="/fps" className='Link'>
+                <button className='navmoba'>
+                        Início
+                </button>
+            </Link>
+            <Link to="/cod" className='Link'>
+                <button className='navmoba'>
+                        Call Of Duty
+                </button>
+            </Link>
+
+            <Link to="/csgo" className='Link'>
+                <button className='navmoba'>
+                    Counter Strike
+                </button>
+            </Link>
+
+            <Link to="/valorante" className='Link'>
+                <button className='navmoba'>
+                    Valorante
+                </button>
+            </Link>
+            
+        </div>
+        <div className='imgs_container'>
+            <div className='imgs1'>
+                <img className='foof' src='https://cdn.key-drop.com/csgo_icons/c8/c89d3288bc252517d1cbdcc1095bfdbd.png' width='290px' height='290px' />
+                <div className='txt_container'>
+                    <div className='txt'>Nice Shote</div>
+                    <div className='txt'>R$210,00</div>
+                </div>
+                <button className='comprar'>ADICIONAR AO CARRINHO</button>
+            </div>
+
+            <div className='imgs1'>
+                <img className='foof' src='https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpovbSsLQJf0ebcZThQ6tCvq4GFqOP9NL7DqWRD6ct2j9bN_Iv9nGu4qgE7NnfxctPGJwZsYV6B8lXtlOy915DpvZicziE3uXEh4inZnxXhgxlEZ-Fvm7XAHohWzUAc/360fx360f' width='290px' height='290px' />
+                <div className='txt_container'>
+                    <div className='txt'>Butterfly Gold</div>
+                    <div className='txt'>R$999,99</div>
+                </div>
+                <button className='comprar'>ADICIONAR AO CARRINHO</button>
+            </div>
+
+            <div className='imgs1'>
+                <img className='foof' src='https://cdn.key-drop.com/csgo_icons/f5/f5a128a4eb50a92ed5364c454b2088a8.png' width='290px' height='290px' />
+                <div className='txt_container'>
+                    <div className='txt'>Spider</div>
+                    <div className='txt'>R$99,99</div>
+                </div>
+                <button className='comprar'>ADICIONAR AO CARRINHO</button>
+            </div>
+        </div>
+        <p> Em breve mais skins.</p>
+    </div>
+</div>
+    )
+}
---
+++ src/Rotas.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Páginas/Home';
+
+//Rotas Battle Royale
+import Battle from './Páginas/Battle Royale';
+import FF from './Páginas/Battle Royale/Páginas/FREEFIRE/Freefire';
+import Fortnite from './Páginas/Battle Royale/Páginas/FORTINITE/Fortnite';
+import PUBG from './Páginas/Battle Royale/Páginas/PUBG/Call';
+
+//Rotas FPS
+import Fps from './Páginas/FPS';
+import COD from './Páginas/FPS/Páginas/COD/COD';
+import CSGO from './Páginas/FPS/Páginas/CSGO/CSGO';
+import Valorant from './Páginas/FPS/Páginas/VALORANTE/VALORANTE';
+
+// ROTAS MOBA
+import Moba from './Páginas/MOBA';
+import LOL from './Páginas/MOBA/Páginas/LOL/LOL';
+import MobileLegends from './Páginas/MOBA/Páginas/MobileLegends/MobileLegends';
+import PokemonUnite from './Páginas/MOBA/Páginas/PokemonUnite/PokemonUnite';
+
+// Rotas RPG
+import Rpg from './Páginas/RPG';
+import Minecraft from './Páginas/RPG/Páginas/Minecraft/Minecraft';
+import Gta from './Páginas/RPG/Páginas/Gta/Gta';
+import Roblox from './Páginas/RPG/Páginas/Roblox/Roblox';
+
+
+export default function Rotas() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fps" element={<Fps />} />
+        <Route path="/cod" element={<COD />} />
+        <Route path="/csgo" element={<CSGO />} />
+        <Route path='/valorante' element={<Valorant />} />
+        <Route path="/battleroyale" element={<Battle />} />
+        <Route path='/Freefire' element={<FF />} />
+        <Route path="/fortnite" element={<Fortnite />} />
+        <Route path="/pubg" element={<PUBG />} />
+        <Route path="/moba" element={<Moba />} />
+        <Route path="/lol" element={<LOL />} />
+        <Route path="/mobilelegends" element={<MobileLegends />} />
+        <Route path="/pokemonunite" element={<PokemonUnite />} />
+        <Route path="/rpg" element={<Rpg />} />
+        <Route path="/mine" element={<Minecraft />} />
+        <Route path='/gta' element={<Gta />} />
+        <Route path='/roblox' element={<Roblox />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
